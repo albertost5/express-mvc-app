37f805083fb9eeeaff4a1f37e15387a9665e2477
albertost5 <sancheztorresalberto@gmail.com>
2022-08-31 16:25:04 +0200

    Modify .env

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ import Sequelize from "sequelize";
 import 'dotenv/config';
 
 
-const db = new Sequelize(process.env.DB_NAME, 'root', '', {
-    host: '127.0.0.1',
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.HOST,
     port: '3306',
     dialect: 'mysql',
     define: {
